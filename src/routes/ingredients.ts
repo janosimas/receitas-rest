07e2b5d7acd8e2a73125c425c2c45e55d7bda872
Jano Simas <janosimas@gmail.com>
2018-06-17 23:58:21 -0300

    convert ingredient name to lower case on insertion

--- src/routes/ingredients.ts
+++ src/routes/ingredients.ts
@@ -7,18 +7,14 @@ export const route = Router();
 route.get('/', async (req: Request, res: Response) => {
   if (!req.query.contains) {
     // full list of ingredients
-    const ingredients = await Ingredient
-      .query()
-      .select('*');
+    const ingredients = await Ingredient.query().select('*');
 
     return res.json(ingredients);
   } else {
     // autocomplete
     console.log(`%${req.query.contains}%`);
-    const ingredients = await Ingredient
-      .query()
-      .select('*')
-      .where('name', 'like', `%${req.query.contains}%`);
+    const ingredients = await Ingredient.query().select('*').where(
+        'name', 'like', `%${req.query.contains}%`);
     return res.json(ingredients);
   }
 });
@@ -27,7 +23,7 @@ route.post('/new', async (req: Request, res: Response) => {
   if (!R.has('body', req) || !R.has('name', req.body)) {
     return res.json({err: 'No ingredient information provided.'});
   }
-  const name: string = req.body.name.trim();
+  const name: string = req.body.name.trim().toLowerCase();
   if (R.isEmpty(name)) {
     return res.json({err: 'Empty ingredient name.'});
   }
